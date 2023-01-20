db722f8e1adfe8d19236ecb9ae116bf7ed489407
Jefrey López <jlopez-2018422@kinal.edu.gt>
2023-01-20 08:55:18 -0600

    Primer commit v1 WebPage

---
+++ js/index.js
@@ -0,0 +1,8 @@
+
+function sumar(){
+    let numero1 = document.getElementById('numero').value;
+    let numero2 = document.getElementById('numerodos').value;
+    
+    let resultado = parseInt(numero1) + parseInt(numero2);
+    console.log(resultado);
+}
